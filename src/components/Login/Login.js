3f183c6ca546836972c1054203414d8c423192f9
Nepsaco <tobie.tsuzuki@gmail.com>
2019-12-24 13:30:19 -0600

    login page finished

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,16 +1,33 @@
 import  React, { Component } from 'react'
 
 export default class Login extends Component{
-    render(){
+    state = {
+        username: '',
+        password: ''
+    }
 
+    changeHandler = (event) => {
+        const name = [event.target.name]
+        const value = event.target.value
+        this.setState({
+            [name]: value
+        })
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault()
+
+    }
+
+    render(){
         return(
             <div className='login'>
-                <form>
+                <form onSubmit>
                     <label>Username</label>
-                    <input type='text' />
+                    <input type='text' name='username' required onChange={this.changeHandler}/>
                     <label>Password</label>
-                    <input />
-                    <input type='submit' value='Login'/>
+                    <input type='text' name='password' required onChange={this.changeHandler}/>
+                    <input className='button' type='submit' value='Login'/>
                 </form>
             </div>
         )
